9dd22d36ca016468c3948685f4849da91495dfbd
Anand24dsu <anandkumaranand384@gmail.com>
2025-03-26 14:36:28 +0530

    jenkins

--- yugabyte/yugabyte.tf
+++ yugabyte/yugabyte.tf
@@ -9,32 +9,27 @@ terraform {
 
 provider "docker" {}
 
-# Define a custom Docker network with a fixed subnet
 resource "docker_network" "yugabyte_network" {
   name   = "yugabyte_network"
   driver = "bridge"
 
   ipam_config {
-    subnet = "192.168.1.0/24" # Define a fixed subnet
+    subnet = "192.168.100.0/24"  # Updated subnet to avoid conflicts
   }
 }
 
-# Pull the YugabyteDB image
 resource "docker_image" "yugabyte" {
-  name         = "yugabytedb/yugabyte:latest"
+  name         = "yugabyte/yugabyte-db:latest"
   keep_locally = false
 }
 
-# Create the YugabyteDB container with a new static IP
 resource "docker_container" "yugabyte" {
   name  = "yugabyte-db"
   image = docker_image.yugabyte.name
   restart = "always"
-
-  # Assign new static IP 192.168.1.200
   networks_advanced {
-    name         = docker_network.yugabyte_network.name
-    ipv4_address = "192.168.1.200"
+    name = docker_network.yugabyte_network.name
+    ipv4_address = "192.168.100.10"  # Assign a fixed IP
   }
 
   ports {
@@ -87,20 +82,18 @@ resource "docker_container" "yugabyte" {
     external = 2222
   }
 
-  # Commands to set up SSH inside the container
   command = [
     "/bin/sh", "-c", 
     "apt-get update && apt-get install -y openssh-server && mkdir -p /run/sshd && echo 'root:root' | chpasswd && sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && service ssh start && tail -f /dev/null"
   ]
 
-  # Allow remote execution inside the container
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
       user        = "root"
       password    = "root"
-      host        = "192.168.1.200"  # Updated static IP
-      port        = 22
+      host        = "192.168.100.10"
+      port        = 2222
     }
 
     inline = [
@@ -109,9 +102,4 @@ resource "docker_container" "yugabyte" {
   }
 
   depends_on = [docker_image.yugabyte, docker_network.yugabyte_network]
-}
-
-# Output the new static IP
-output "container_ip" {
-  value = "192.168.1.200"
 }
